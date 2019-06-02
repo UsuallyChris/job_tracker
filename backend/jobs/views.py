23184f39192505d6cb582ad96d01ddfb3850298d
UsuallyChris <schoonc@gmail.com>
2019-06-01 20:47:09 -0700

    Defined ViewSets for Job and Contact models

--- backend/jobs/views.py
+++ backend/jobs/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+""" view definitions for jobs app """
+from rest_framework import viewsets, permissions
+from .models import Job, Contact
+from .serializers import JobSerializer, ContactSerializer
 
-# Create your views here.
+
+class JobViewSet(viewsets.ModelViewSet):
+    """ ViewSet definition for Job model """
+    queryset = Job.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = JobSerializer
+
+
+class ContactViewSet(viewsets.ModelViewSet):
+    """ ViewSet definition for Contact model """
+    queryset = Contact.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = ContactSerializer
