d1828254c95f10fd7ee45715305fcd4836dabdeb
UsuallyChris <schoonc@gmail.com>
2019-06-01 20:33:04 -0700

    Created Job model

--- backend/jobs/models.py
+++ backend/jobs/models.py
@@ -1,3 +1,9 @@
+""" model definitions for jobs app """
 from django.db import models
 
-# Create your models here.
+
+class Job(models.Model):
+    """ Job model definition """
+    company = models.CharField(max_length=100)
+    date_applied = models.DateField()
+    job_title = models.CharField(max_length=100)
