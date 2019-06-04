dc8d573b8c802e593a360c0f8da24d106bb92517
UsuallyChris <schoonc@gmail.com>
2019-06-03 23:18:06 -0700

    Fixed some right side content positioning

--- frontend/src/components/layout/right/Jobs.jsx
+++ frontend/src/components/layout/right/Jobs.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
 function Jobs() {
+
   return(
     <div className="right-container">
       <div className="right-content-container">
-        <div className="title">
-          <h1>Jobs</h1>
-        </div>
         <div className="card-container">
           <h2>Card Container</h2>
         </div>
