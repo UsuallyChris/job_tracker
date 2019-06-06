568b3f8cdb9583a76120c07c27c148b79982e36d
UsuallyChris <schoonc@gmail.com>
2019-06-05 17:23:01 -0700

    JobDetail correctly pulling the right job from api and updating state

--- frontend/src/components/layout/content/JobDetail.jsx
+++ frontend/src/components/layout/content/JobDetail.jsx
@@ -4,25 +4,51 @@ import { Link } from 'react-router-dom';
 // Component Imports
 import MainContainer from '../MainContainer';
 import TitleBar from '../../common/TitleBar';
+import axios from 'axios';
 
 class JobDetail extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      id: '',
+      job_title: '',
+      company: '',
+      date_applied: '',
+      status: ''
     }
+  }
+
+  componentDidMount() {
+    axios.get(`http://127.0.0.1:8000/api/jobs/${this.props.match.params.id}/`)
+      .then(res => {
+        this.setState({
+          id: res.data.id,
+          job_title: res.data.job_title,
+          company: res.data.company,
+          date_applied: res.data.date_applied,
+          status: res.status
+        });
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err.response);
+        this.setState({
+          status: err.response.status
+        })
+      })
   }
 
   render() {
     return(
       <MainContainer>
         <TitleBar title='Job Detail'>
-        <Link to='/jobs'>
-          <div className="link-button">
-            <h3>Back to Jobs</h3>
-          </div>
-        </Link>
+          <Link to='/jobs'>
+            <div className="link-button">
+              <h3>Back to Jobs</h3>
+            </div>
+          </Link>
         </TitleBar>
+
       </MainContainer>
     );
   }
