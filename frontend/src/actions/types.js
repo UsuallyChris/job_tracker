66e92b2455cf22f5c2181c9bb24f214b92f3682c
UsuallyChris <schoonc@gmail.com>
2019-06-06 02:15:33 -0700

    Updated types

--- frontend/src/actions/types.js
+++ frontend/src/actions/types.js
@@ -1,8 +1,12 @@
 // JOBS ACTIONS
 export const GET_JOBS = 'GET_JOBS';
 export const DELETE_JOB = 'DELETE_JOB';
+export const ADD_JOB = 'ADD_JOB';
+export const UPDATE_JOB = 'UPDATE_JOB';
 
 
 // CONTACTS ACTIONS
 export const GET_CONTACTS = 'GET_CONTACTS';
 export const DELETE_CONTACT = 'DELETE_CONTACT';
+export const ADD_CONTACT = 'ADD_CONTACT';
+export const UPDATE_CONTACT = 'UPDATE_CONTACT';
