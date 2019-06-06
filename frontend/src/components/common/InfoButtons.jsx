c575fc70e3595ccd33b82ea0e3f2905610e1eca5
UsuallyChris <schoonc@gmail.com>
2019-06-05 23:14:27 -0700

    Deleting contacts and jobs working correctly

--- frontend/src/components/common/InfoButtons.jsx
+++ frontend/src/components/common/InfoButtons.jsx
@@ -1,15 +1,47 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import axios from 'axios';
 
-function InfoButtons(props) {
-  return(
-    <div className="info-buttons">
-      <Link to={`/${props.url}/${props.id}/update`}>Update {props.model}</Link>
-      <div className="delete-button">
-        <p>Delete {props.model}</p>
+class InfoButtons extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      to_dashboard: false
+    }
+
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    axios.delete(`http://127.0.0.1:8000/api/${this.props.url}/${this.props.id}/`)
+      .then(res => {
+        console.log(res)
+        this.setState({
+          to_dashboard: true
+        })
+      })
+      .catch(err => {
+        console.log(err.response)
+      })
+  }
+
+  render() {
+
+    if(this.state.to_dashboard === true) {
+      return(
+        <Redirect exact to={`/${this.props.url}`} />
+      )
+    }
+
+    return(
+      <div className="info-buttons">
+        <Link to={`/${this.props.url}/${this.props.id}/update`}>Update {this.props.model}</Link>
+        <div className="delete-button">
+          <button onClick={this.onClick}>Delete {this.props.model}</button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default InfoButtons;
