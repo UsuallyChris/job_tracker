b2e0d4e1d52ccfda23f446d8b1bf4e4a9d83ce6e
UsuallyChris <schoonc@gmail.com>
2019-06-26 22:26:18 -0700

    Dynamically colored border based on job status working correctly.

--- frontend/src/components/common/JobCard.jsx
+++ frontend/src/components/common/JobCard.jsx
@@ -4,16 +4,20 @@ import { Link } from 'react-router-dom';
 function JobCard(props) {
 
   let job_status = '';
+  let job_status_style = '';
   if (props.job_status === 'APP') {
-    job_status = "Applied";
+    job_status = 'Applied';
+    job_status_style = 'applied'
   } else if (props.job_status === 'OFF') {
-    job_status = "Job Offered";
+    job_status = 'Job Offered';
+    job_status_style = 'offered'
   } else {
-    job_status = "Rejected";
+    job_status = 'Rejected';
+    job_status_style = 'rejected'
   }
 
   return(
-    <Link to={`/jobs/${props.id}`} className={`card card-shadow`}>
+    <Link to={`/jobs/${props.id}`} className={`card card-shadow ${job_status_style}`}>
       <div>
         <h2>{props.job_title} at {props.company}</h2>
         <div className="job-card-status">
