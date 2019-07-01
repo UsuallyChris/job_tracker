ad29411de7d4fdf58d362a3c4ec462d894c9d86f
UsuallyChris <schoonc@gmail.com>
2019-06-30 22:44:55 -0700

    Changed date format to get rid of leading zero.

--- frontend/src/components/layout/content/AddJob.jsx
+++ frontend/src/components/layout/content/AddJob.jsx
@@ -79,7 +79,7 @@ class AddJob extends Component {
                 selected={this.state.date_applied} 
                 placeholderText='Date Applied'
                 allowSameDay={true}
-                dateFormat="MMMM dd, yyyy"
+                dateFormat="MMMM d, yyyy"
                 onChange={this.onChangeDate}
                 value={this.state.date_applied}
               />
--- frontend/src/components/layout/content/UpdateJob.jsx
+++ frontend/src/components/layout/content/UpdateJob.jsx
@@ -96,7 +96,7 @@ class UpdateJob extends Component {
                 selected={this.state.date_applied} 
                 placeholderText='Date Applied'
                 allowSameDay={true}
-                dateFormat="MMMM dd, yyyy"
+                dateFormat="MMMM d, yyyy"
                 onChange={this.onChangeDate}
                 value={this.state.date_applied}
               />
