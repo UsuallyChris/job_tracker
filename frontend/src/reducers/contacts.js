a42014c440b1cf72f54939817bf98b312c8be750
UsuallyChris <schoonc@gmail.com>
2019-07-13 16:38:14 -0700

    Contacts are sorted at every stage in reducers.

--- frontend/src/reducers/contacts.js
+++ frontend/src/reducers/contacts.js
@@ -9,19 +9,19 @@ export default function(state = initialState, action) {
     case GET_CONTACTS:
       return{
         ...state,
-        contacts: action.payload
+        contacts: action.payload.sort( (a,b) => a.id < b.id)
       };
 
     case DELETE_CONTACT:
       return{
         ...state,
-        contacts: state.contacts.filter(contact => contact.id !== action.payload)
+        contacts: state.contacts.filter(contact => contact.id !== action.payload).sort( (a,b) => a.id < b.id)
       };
 
     case ADD_CONTACT:
         return{
           ...state,
-          contacts: [...state.contacts, action.payload]
+          contacts: [...state.contacts, action.payload].sort( (a,b) => a.id < b.id)
         }
 
     default:
