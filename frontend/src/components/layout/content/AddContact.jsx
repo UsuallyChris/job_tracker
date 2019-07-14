3f78a67f5acbae803e57051a61e7c6fc91a38ebc
UsuallyChris <schoonc@gmail.com>
2019-07-13 18:30:46 -0700

    Formik on AddContact working.

--- frontend/src/components/layout/content/AddContact.jsx
+++ frontend/src/components/layout/content/AddContact.jsx
@@ -9,35 +9,15 @@ import { addContact } from '../../../actions/contacts';
 import MainContainer from '../MainContainer';
 import TitleBar from '../../common/TitleBar';
 
+// Formik Imports
+import { Formik, Form, Field } from 'formik';
+
 class AddContact extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      company: '',
-      phone_number: '',
-      email: '',
       to_dashboard: false
     }
-  
-  this.onSubmit = this.onSubmit.bind(this);
-  this.onChange = this.onChange.bind(this);
-  }
-
-  onChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  }
-
-  onSubmit(e) {
-    e.preventDefault();
-    const {name, company, phone_number, email} = this.state;
-    const contact = {name, company, phone_number, email};
-    this.props.addContact(contact);
-    this.setState({
-      to_dashboard: true
-    })
   }
 
   render() {
@@ -58,20 +38,40 @@ class AddContact extends Component {
           </Link>
         </TitleBar>
         <div className="form-container">
-          <div className="form-card card-shadow">
-            <form onSubmit={this.onSubmit}>
-              <h2>Name:</h2>
-              <input type="text" name="name" onChange={this.onChange} value={this.state.name}/>
-              <h2>Company:</h2>
-              <input type="text" name="company" onChange={this.onChange} value={this.state.company}/>
-              <h2>Phone Number:</h2>
-              <input type="text" name="phone_number" onChange={this.onChange} value={this.state.phone_number}/>
-              <h2>Email:</h2>
-              <input type="text" name="email" onChange={this.onChange} value={this.state.email}/>
-              <div className="form-button">
-                <button type="submit">Add Contact</button>
-              </div>
-            </form>        
+          <div className="form-card shadow">
+            <Formik
+              initialValues={{
+                name: '',
+                company: '',
+                phone_number: '',
+                email: ''
+              }}
+              onSubmit={(values, { setSubmitting }) => {
+                const { name, company, phone_number, email } = values;
+                const contact = { name, company, phone_number, email }
+                this.props.addContact(contact);
+                setSubmitting(false);
+                this.setState({
+                  to_dashboard: true
+                })
+              }}
+            >
+              {({ isSubmitting }) => (
+                <Form>
+                  <h2>Name:</h2>
+                  <Field type="text" name="name" />
+                  <h2>Company:</h2>
+                  <Field type="text" name="company" />
+                  <h2>Phone Number:</h2>
+                  <Field type="text" name="phone_number" />
+                  <h2>Email:</h2>
+                  <Field type="text" name="email" />
+                  <div className="form-button">
+                    <button type="submit" disabled={isSubmitting}>Add Contact</button>
+                  </div>
+                </Form>
+              )}
+            </Formik>
           </div>
         </div>
       </MainContainer>
