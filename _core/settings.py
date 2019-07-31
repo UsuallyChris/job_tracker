5e1249ddcb261780c647877e816d603c47ec95cb
UsuallyChris <schoonc@gmail.com>
2019-07-30 19:07:14 -0700

    Updated WhiteNoise implementation. Was using old documentation.

--- _core/settings.py
+++ _core/settings.py
@@ -53,6 +53,9 @@ MIDDLEWARE = [
     # CORS MIDDLEWARE
     'corsheaders.middleware.CorsMiddleware',
 
+    # WhiteNoise
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -132,7 +135,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # CORS ORIGIN WHITELIST
 CORS_ORIGIN_WHITELIST = [
